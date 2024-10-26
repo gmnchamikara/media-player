e1e2afde84185a9706c8f00f0299032561f89308
Nuwan Chamikara <www.gmnuwanchamikara@gmail.com>
2024-10-26 22:36:08 +0530

    Initial commit

---
+++ constants/images.ts
@@ -0,0 +1,6 @@
+import unknownArtistImage from '@/assets/images/unknown_artist.png'
+import unknownTrackImage from '@/assets/images/unknown_track.png'
+
+// Directly exporting the assets using `require`
+export const unknownTrackImageUri = unknownTrackImage
+export const unknownArtistImageUri = unknownArtistImage
